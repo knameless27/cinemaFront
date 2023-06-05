661831960702aca8d4afaabc429afd098024d3b2
unknown <knameless27@gmail.com>
2023-06-05 10:29:51 -0500

    feat: login

--- src/service/auth/auth.js
+++ src/service/auth/auth.js
@@ -1,11 +1,32 @@
-const paginaCustom = (data) => {
+import axios from "axios";
+
+const login = (data) => {
     return new Promise((resolve, reject) => {
-        const token = localStorage.getItem('accessToken');
+        // const token = localStorage.getItem('accessToken');
         const headers = {
-            Authorization: `Bearer ${token}`
+            // Authorization: `Bearer ${token}`
         };
         axios
-            .post(`${import.meta.env.VITE_APP_BACK}/ver_caja_global`, data, {
+            .post(`${import.meta.env.VITE_APP_BACK}auth/login`, data, {
+                headers: headers
+            })
+            .then((resp) => {
+                resolve(resp.data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+const register = (data) => {
+    return new Promise((resolve, reject) => {
+        // const token = localStorage.getItem('accessToken');
+        const headers = {
+            // Authorization: `Bearer ${token}`
+        };
+        axios
+            .post(`${import.meta.env.VITE_APP_BACK}auth/register`, data, {
                 headers: headers
             })
             .then((resp) => {
@@ -18,5 +39,6 @@ const paginaCustom = (data) => {
 };
 
 export default {
-    paginaCustom
+    login,
+    register
 };
--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -14,7 +14,8 @@
                         <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true"
                             class="w-full mb-3" inputClass="w-full"></Password>
                         <div>
-                            <Button label="Registrame!" link class="w-full p-2 text-xl" @click="registro = !registro"></Button>
+                            <Button label="Registrame!" link class="w-full p-2 text-xl"
+                                @click="registro = !registro"></Button>
                         </div>
                         <div class="pt-5">
                             <Button label="Iniciar sesion" class="w-full p-3 text-xl" @click="iniciarSesion"></Button>
@@ -23,18 +24,19 @@
                     <div v-else>
                         <label for="dni1" class="block text-900 text-xl font-medium mb-2">DNI</label>
                         <InputText id="dni1" type="text" placeholder="dni address" class="w-full md:w-30rem mb-5"
-                        style="padding: 1rem" v-model="dni" />
+                            style="padding: 1rem" v-model="dni" />
 
                         <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
                         <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true"
-                        class="w-full mb-3" inputClass="w-full"></Password>
+                            class="w-full mb-3" inputClass="w-full"></Password>
 
                         <label for="password1" class="block text-900 font-medium text-xl mb-2">Confirm password</label>
                         <Password id="password1" v-model="confirmPassword" placeholder="Confirm Password" :toggleMask="true"
-                        class="w-full mb-3" inputClass="w-full"></Password>
+                            class="w-full mb-3" inputClass="w-full"></Password>
 
                         <div>
-                            <Button label="Iniciar sesion!" link class="w-full p-2 text-xl" @click="registro = !registro"></Button>
+                            <Button label="Iniciar sesion!" link class="w-full p-2 text-xl"
+                                @click="registro = !registro"></Button>
                         </div>
                         <div class="pt-5">
                             <Button label="Registrarse!" class="w-full p-3 text-xl" @click="registrarse"></Button>
@@ -43,15 +45,20 @@
                 </div>
             </div>
         </div>
+        <Toast />
     </div>
 </template>
 
 <script>
 import { useLayout } from '@/layout/composables/layout';
+// import {useToast} from 'primevue/usetoast';
+import axios from '@/service/auth/auth.js';
 
 const { layoutConfig } = useLayout();
 
 export default {
+    components: {
+    },
     data() {
         return {
             logoUrl: `layout/images/${layoutConfig.darkTheme.value ? 'logo-white' : 'logo-dark'}.svg`,
@@ -64,10 +71,43 @@ export default {
     },
     methods: {
         iniciarSesion() {
-            console.log(import.meta.env.VITE_APP_BACK);
+            if (this.dni.length < 6) return this.$toast.add({ severity: 'error', summary: 'Error', detail: 'El dni tiene que ser mayor a 6 caracteres', life: 3000 })
+            if (this.password.length < 5) return this.$toast.add({ severity: 'error', summary: 'Error', detail: 'La contraseña tiene que ser mayor a 4 caracteres', life: 3000 })
+            if (this.password.length > 12) return this.$toast.add({ severity: 'error', summary: 'Error', detail: 'La contraseña tiene que ser menor a 13 caracteres', life: 3000 })
+            const data = {
+                dni: this.dni,
+                password: this.password
+            }
+            axios.login(data).then(async ({ status, message }) => {
+                switch (status) {
+                    case 'error':
+                        this.$toast.add({ severity: status, summary: 'Error', detail: message, life: 3000 })
+                        break;
+                    default:
+                        this.$toast.add({ severity: status, summary: 'Success', detail: message, life: 3000 })
+                        break;
+                }
+            })
         },
         registrarse() {
-            console.log(import.meta.env.VITE_APP_BACK);
+            if (this.dni.length < 6) return this.$toast.add({ severity: 'error', summary: 'Error', detail: 'El dni tiene que ser mayor a 6 caracteres', life: 3000 })
+            if (this.password.length < 5) return this.$toast.add({ severity: 'error', summary: 'Error', detail: 'La contraseña tiene que ser mayor a 4 caracteres', life: 3000 })
+            if (this.password.length > 12) return this.$toast.add({ severity: 'error', summary: 'Error', detail: 'La contraseña tiene que ser menor a 13 caracteres', life: 3000 })
+            if (this.password != this.confirmPassword) return this.$toast.add({ severity: 'error', summary: 'Error', detail: 'Las contraseñas no coinciden', life: 3000 })
+            const data = {
+                dni: this.dni,
+                password: this.password
+            }
+            axios.login(data).then(async ({ status, message }) => {
+                switch (status) {
+                    case 'error':
+                        this.$toast.add({ severity: status, summary: 'Error', detail: message, life: 3000 })
+                        break;
+                    default:
+                        this.$toast.add({ severity: status, summary: 'Success', detail: message, life: 3000 })
+                        break;
+                }
+            })
         }
     },
 }
@@ -82,4 +122,5 @@ export default {
 .pi-eye-slash {
     transform: scale(1.6);
     margin-right: 1rem;
-}</style>
+}
+</style>
